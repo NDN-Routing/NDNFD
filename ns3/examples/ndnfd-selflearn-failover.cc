fa7973983a1b0c67793e1a130e0d05a4f7c32ecf
Junxiao Shi <git@mail1.yoursunny.com>
2013-04-05 16:49:39 -0700

    selflearn-failover scenario: let it stablize

--- ns3/examples/ndnfd-selflearn-failover.cc
+++ ns3/examples/ndnfd-selflearn-failover.cc
@@ -14,12 +14,12 @@ A----B----C--+         |
      |        \        |
      H         ---F----G
 
-Consumer A expresses 2 Interests per second for 0.0 ~ 12.0 seconds.
-Producer D runs for  0.0 ~ 3.0 seconds.
-Producer F runs for  2.9 ~ 6.0 seconds.
-Producer E runs for  5.9 ~ 9.0 seconds.
-Producer G runs for  8.9 ~11.0 seconds.
-Producer H runs for  0.0 ~ 1.0 seconds.
+Consumer A expresses 5 Interests per second for 0.0 ~ 120.0 seconds.
+Producer D runs for  0.0 ~ 30.0 seconds.
+Producer F runs for 29.9 ~ 60.0 seconds.
+Producer E runs for 59.9 ~ 90.0 seconds.
+Producer G runs for 89.9 ~110.0 seconds.
+Producer H runs for  0.0 ~ 10.0 seconds.
 
 */
 
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   ns3::Config::SetDefault("ns3::CsmaChannel::DataRate", ns3::StringValue("1Mbps"));
   ns3::Config::SetDefault("ns3::CsmaChannel::Delay", ns3::StringValue("10ms"));
   ns3::Config::SetDefault("ns3::DropTailQueue::MaxPackets", ns3::StringValue("20"));
-  ns3::Config::SetDefault("ns3::ndn::Consumer::RetxTimer", ns3::StringValue("60s"));
+  ns3::Config::SetDefault("ns3::ndn::Consumer::RetxTimer", ns3::StringValue("300s"));
 
   ns3::NodeContainer nodes;
   nodes.Create(8);
@@ -49,25 +49,25 @@ int main(int argc, char *argv[]) {
 
   ns3::ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix("/prefix");
-  consumerHelper.SetAttribute("Frequency", ns3::StringValue("2"));
+  consumerHelper.SetAttribute("Frequency", ns3::StringValue("5"));
   ns3::ApplicationContainer consumerA = consumerHelper.Install(nodes.Get(0));//A
-  consumerA.Start(ns3::Seconds(0.0)); consumerA.Stop(ns3::Seconds(12.0));
+  consumerA.Start(ns3::Seconds(0.0)); consumerA.Stop(ns3::Seconds(120.0));
 
   ns3::ndn::AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix("/prefix");
   producerHelper.SetAttribute("PayloadSize", ns3::StringValue("1024"));
   ns3::ApplicationContainer producerD = producerHelper.Install(nodes.Get(3));
-  producerD.Start(ns3::Seconds(0.0)); producerD.Stop(ns3::Seconds(3.0)); 
+  producerD.Start(ns3::Seconds(0.0)); producerD.Stop(ns3::Seconds(30.0)); 
   ns3::ApplicationContainer producerE = producerHelper.Install(nodes.Get(4));
-  producerE.Start(ns3::Seconds(2.9)); producerE.Stop(ns3::Seconds(6.0)); 
+  producerE.Start(ns3::Seconds(29.9)); producerE.Stop(ns3::Seconds(60.0)); 
   ns3::ApplicationContainer producerF = producerHelper.Install(nodes.Get(5));
-  producerF.Start(ns3::Seconds(5.9)); producerF.Stop(ns3::Seconds(9.0)); 
+  producerF.Start(ns3::Seconds(59.9)); producerF.Stop(ns3::Seconds(90.0)); 
   ns3::ApplicationContainer producerG = producerHelper.Install(nodes.Get(6));
-  producerG.Start(ns3::Seconds(8.9)); producerG.Stop(ns3::Seconds(11.0));
+  producerG.Start(ns3::Seconds(89.9)); producerG.Stop(ns3::Seconds(110.0));
   ns3::ApplicationContainer producerH = producerHelper.Install(nodes.Get(7));
-  producerH.Start(ns3::Seconds(0.0)); producerH.Stop(ns3::Seconds(1.0));
+  producerH.Start(ns3::Seconds(0.0)); producerH.Stop(ns3::Seconds(10.0));
 
-  ns3::Simulator::Stop(ns3::Seconds(12.5));
+  ns3::Simulator::Stop(ns3::Seconds(122.0));
   ns3::Simulator::Run();
   ns3::Simulator::Destroy();
 
