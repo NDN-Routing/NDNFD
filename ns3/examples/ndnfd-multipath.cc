0c26d9dd6c765c19816cc20de80c0bc76b38beb4
Junxiao Shi <git@mail1.yoursunny.com>
2013-08-10 11:19:18 -0700

    multipath simulation: separate B,C,G CsmaChannel
    Put B-C and B-G in two separate CsmaChannels, so that non self-learning strategy could choose to send to either upper or lower link. This results in a fair comparison.

--- ns3/examples/ndnfd-multipath.cc
+++ ns3/examples/ndnfd-multipath.cc
@@ -10,11 +10,11 @@ This example demostrates self-learning strategy can make use of multiple paths d
 and uses shorter paths during low traffic.
 It only works when NDNFD is compiled with SelfLearnStrategy.
 
-          ---C----D----E---
-         /                 \
-A----B--+                  F
-         \                 /
-          ---G---------H---
+       ---C----D----E---
+      /                 \
+A----B                  F
+      \                 /
+       ---G---------H---
 
 Producer F is always available.
 Consumer A expresses 5 Interests/s, and 100 Interests/s during 30.0-60.0s.
@@ -39,7 +39,8 @@ int main(int argc, char *argv[]) {
     node G
     node H
     csma 900Kbps 6560ns A B
-    csma 900Kbps 6560ns B C G
+    csma 900Kbps 6560ns B C
+    csma 900Kbps 6560ns B G
     csma 600Kbps 6560ns C D
     csma 600Kbps 6560ns D E
     csma 600Kbps 6560ns E F
@@ -51,18 +52,21 @@ int main(int argc, char *argv[]) {
   ns3::ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix("/prefix");
   consumerHelper.SetAttribute("Frequency", ns3::StringValue("5"));
+  consumerHelper.SetAttribute("StartSeq", ns3::IntegerValue(0));
+  consumerHelper.SetAttribute("MaxSeq", ns3::IntegerValue(0+5*90));
   ns3::ApplicationContainer consumerA = consumerHelper.Install(ns3::Names::Find<ns3::Node>("A"));
-  consumerA.Start(ns3::Seconds(0.0)); consumerA.Stop(ns3::Seconds(90.0));
+  consumerA.Start(ns3::Seconds(0.0)); consumerA.Stop(ns3::Seconds(95.0));
   consumerHelper.SetAttribute("Frequency", ns3::StringValue("95"));
-  consumerHelper.SetAttribute("StartSeq", ns3::StringValue("1000"));
+  consumerHelper.SetAttribute("StartSeq", ns3::IntegerValue(1000));
+  consumerHelper.SetAttribute("MaxSeq", ns3::IntegerValue(1000+95*30));
   ns3::ApplicationContainer consumerA2 = consumerHelper.Install(ns3::Names::Find<ns3::Node>("A"));
-  consumerA2.Start(ns3::Seconds(30.0)); consumerA2.Stop(ns3::Seconds(60.0));
+  consumerA2.Start(ns3::Seconds(30.0)); consumerA2.Stop(ns3::Seconds(65.0));
 
   ns3::ndn::AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix("/prefix");
   producerHelper.SetAttribute("PayloadSize", ns3::StringValue("1024"));
   ns3::ApplicationContainer producerF = producerHelper.Install(ns3::Names::Find<ns3::Node>("F"));
-  producerF.Start(ns3::Seconds(0.0)); producerF.Stop(ns3::Seconds(90.0));
+  producerF.Start(ns3::Seconds(0.0)); producerF.Stop(ns3::Seconds(95.0));
   
   auto delay_tracers = ns3::ndn::AppDelayTracer::InstallAll("ndnfd-multipath_delay.tsv");
   ns3::Ptr<ndnfd::MessageCounter> message_counter = ns3::Create<ndnfd::MessageCounter>("ndnfd-multipath_msgcount.tsv");
@@ -72,7 +76,7 @@ int main(int argc, char *argv[]) {
   ns3::Ptr<ndnfd::MessageCounter> message_counterH = ns3::Create<ndnfd::MessageCounter>("ndnfd-multipath_msgcountH.tsv");
   message_counterH->ConnectNode(ns3::Names::Find<ns3::Node>("H"));
 
-  ns3::Simulator::Stop(ns3::Seconds(92.0));
+  ns3::Simulator::Stop(ns3::Seconds(95.0));
   ns3::Simulator::Run();
   ns3::Simulator::Destroy();
 
